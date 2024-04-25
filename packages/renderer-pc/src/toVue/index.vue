a99516edf1738fc30c1bc7f180f8ae7ff5ee7d5e
GodStream <zpqwangyiyouxiang@163.com>
2024-04-25 20:33:55 +0800

    feat: 支持vue运行时

---
+++ packages/renderer-pc/src/toVue/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <renderer-core
+    ref="currentRef"
+    :json="json"
+    :props="props"
+    :config="config"
+    :comDefs="comDefs"
+  />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { applyPureReactInVue } from 'veaury'
+import Core from '../core/index.tsx'
+
+const RendererCore = applyPureReactInVue(Core)
+
+const { props, config, json, comDefs } = defineProps(['props', 'config', 'json', 'comDefs'])
+
+const currentRef = ref()
+
+// onMounted((props) => {
+//   console.log(props)
+//   setTimeout(() => {
+//     console.log(currentRef.value.__veauryReactRef__)
+//   }, 0);
+// })
+defineExpose({
+  currentRef
+})
+
+
+// const onClick = (currentRef) => {
+//   console.log(currentRef.__veauryReactRef__)
+// }
+
+
+
+// export default {
+//   props: ['props', 'config', 'json', 'comDefs'],
+//   components: {
+//     Renderer: applyPureReactInVue(RendererComponent)
+//   },
+//   setup(props) {
+//     const currentRef = ref()
+
+//     const onClick = (currentRef) => {
+//       console.log(currentRef.__veauryReactRef__)
+//     }
+//     onMounted(() => {
+//       console.log(currentRef, currentRef.__veauryReactRef__)
+//     })
+//     return {
+//       currentRef,
+//       onClick
+//     }
+//   }
+// }
+
+</script>
