8c1ee6589ffc9ad2642ee697f4abe08ce4d38391
GodStream <zpqwangyiyouxiang@163.com>
2024-05-23 21:33:33 +0800

    ref: update

--- packages/renderer-pc/types/index.d.ts
+++ packages/renderer-pc/types/index.d.ts
@@ -1,2 +1,15 @@
-import Core from './core/index';
-export default Core;
+/// <reference types="react" />
+import { Renderer, Canvas, Slot, Component, Module } from './react';
+import { compareVersionLatest } from './utils';
+declare const version: string;
+export { Renderer, Canvas, Slot, Component, Module, version, compareVersionLatest, };
+declare const _default: {
+  Renderer: import("react").ForwardRefExoticComponent<import("./react").RendererProps & import("react").RefAttributes<unknown>>;
+  Canvas: typeof Canvas;
+  Slot: typeof Slot;
+  Component: typeof Component;
+  Module: typeof Module;
+  version: string;
+  compareVersionLatest: (v1: string, v2: string) => number;
+};
+export default _default;
